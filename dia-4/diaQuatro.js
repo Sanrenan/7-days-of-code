a7478c3db08f4c6f0fc2f73d00e0ea39c53f94a3
Renan <j.renan.r@gmail.com>
2022-03-28 10:27:14 -0300

    dias 4 e 5 concluidos

---
+++ dia-4/diaQuatro.js
@@ -0,0 +1,16 @@
+const numeroSecreto = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let chute = "";
+let acertou = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    chute = prompt("Tente adivinhar o número de 0 a 10:");
+    if(chute == numeroSecreto){
+        alert(`Parabéns, você acertou! O número era ${numeroSecreto}.`);
+        acertou = true;
+        break;
+    }
+    alert("Errado!");
+}
+if(!acertou){
+    alert(`Infelizmente, você não acertou. O número era ${numeroSecreto}!`);
+}
